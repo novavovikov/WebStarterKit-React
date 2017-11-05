5a409a4ebc637b5d7d119dc1354836d68364c939
Nova Vovikov <novavovikov@gmail.com>
2017-11-05 18:20:54 +0300

    fix

---
+++ config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports.ENV = {
+	apiUrl: '/api',
+	apiHost: 'localhost',
+	apiPort: 3012,
+
+	port: process.env.PORT || 3000,
+
+	webpackHost: 'localhost',
+	webpackPort: 9000,
+
+	dbURL: 'mongodb://localhost:27017/api'
+};
+
+module.exports.PATH = {
+	public: path.join(__dirname, 'public'),
+	dist: path.join(__dirname, 'views'),
+	client: path.join(__dirname, 'client'),
+	server: path.join(__dirname, 'server'),
+};
